4eb508f55833adce3da253b9c42d7ac63fd4a755
toshak garg <toshakgarg2005@gmail.com>
2024-03-18 22:47:47 +0530

    changes in https://youtu.be/fdubeMFwuGs?si=Pbi3N-ZxRRMkb2nLupr triangle

--- codes/operators/looping/for/pattern/Triangle_upper.py
+++ codes/operators/looping/for/pattern/Triangle_upper.py
@@ -6,3 +6,7 @@ for i in range(1,n+1):
     for j in range(2*i-1):
         print("*",end=" ")
     print()
+
+n=int(input("Enter number of rows: "))
+for i in range(n):
+    print(' ' * (n - i - 1) + ' * ' * (2 * i + 1))
